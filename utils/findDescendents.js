24488524a44c7023e4c60ed6eaddf276fc431401
Audun Bjørnerud Mo <audun.bjornerud.mo@finn.no>
2019-09-09 20:36:59 +0200

    fix: simplify recursive finder (45 min)

--- utils/findDescendents.js
+++ utils/findDescendents.js
@@ -1,41 +1,17 @@
-const csv = require("csvtojson")
-
 /*
  * Recursively constructs a list of all descendents of a particular node.
- * This solution exploits the pairwise string representation of the models,
- * going one layer down at each recursion until it can find no more children
+ *
+ * @param {String} nodeId
+ * @param {Array} nodePairs, expects form [{ parentId: String, childId: String }]
  */
-const findDescendents = (node, parentArray, childArray, nodesFound = []) => {
-  if (parentArray.indexOf(node) === -1 && childArray.indexOf(node) === -1) {
-    throw `${node} is not a valid node`
+const findDescendents = (nodeId, nodePairs) => {
+  const parentNodes = nodePairs.filter(row => row.parentId === nodeId)
+  const children = parentNodes.map(parent => parent.childId)
+
+  if (children.length > 0) {
+    return [...children, ...children.map(child => findDescendents(child, nodePairs)).flat()]
   }
-  const children = parentArray
-    .map((parent, index) => index)
-    .filter(index => parentArray[index] === node)
-    .map(index => childArray[index])
-    .filter(child => nodesFound.indexOf(child) === -1)
-
-  const found = [...nodesFound, ...children]
-
-  if (children.length === 0) {
-    return node
-  } else if (children.length === 1) {
-    return children.map(child => findDescendents(child, parentArray, childArray, found))
-  } else {
-    return [...children, ...children.map(child => findDescendents(child, parentArray, childArray, found))].flat()
-  }
-}
-
-const parseCSV = async csvString => {
-  const jsonified = await csv()
-    .fromString(csvString)
-    .then(res => {
-      return res
-    })
-  const parents = jsonified.map(row => row.parentId)
-  const children = jsonified.map(row => row.childId)
-  return [parents, children]
+  return nodeId
 }
 
 module.exports = findDescendents
-module.exports.parseCSV = parseCSV
--- utils/findDescendents.test.js
+++ utils/findDescendents.test.js
@@ -1,15 +1,18 @@
-const findDescendents = require('./findDescendents')
 const fs = require('fs')
 const path = require('path')
-const parseCSV = require('./findDescendents').parseCSV
+const csv = require('csvtojson')
+
+const findDescendents = require('./findDescendents')
+const makeUnique = require('./makeUnique')
 
 test('should correctly find the descendents', async (done) => {
   expect.assertions(1)
-  const testArray = fs.readFileSync(path.resolve(__dirname, 'data.csv')).toString()
+  const testCsvData = fs.readFileSync(path.resolve(__dirname, '..', 'data.csv')).toString()
 
-  const [parents, children] = await parseCSV(testArray)
+  const nodePairs = await csv().fromString(testCsvData)
+
   const expectedResult = ['E', 'F', 'H', 'I']
-  const result = findDescendents('C', parents, children)
+  const result = makeUnique(findDescendents('C', nodePairs))
   expect(result).toEqual(expectedResult)
   done()
 })
