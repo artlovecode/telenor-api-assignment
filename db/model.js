813935f4ba5733c81cb033dc464a67c489a158da
Audun Bjørnerud Mo <audun.bjornerud.mo@finn.no>
2019-09-09 20:39:23 +0200

    feat: update model to work with new parser and traverser (5 min)

--- db/model.js
+++ db/model.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose")
 
+const childParentPairSchema = new mongoose.Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  parentId: String,
+  childId: String
+})
+
 const dataModelSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now()
   },
-  model: {
-    type: String,
-    validate: {
-      validator(model) {
-        return model.split("\n").every(str => /\w+,\w+/.test(str))
-      }
-    }
-  },
+  model: [childParentPairSchema]
 })
 
 const DataModel = mongoose.model("DataModel", dataModelSchema)
